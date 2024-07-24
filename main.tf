965a2c8346eb3bc3bd37bab15bafb85dad85a2cd
matikaa <mateukar@corp.idemia.com>
2024-07-24 16:46:20 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,55 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "grupa-2-apps"
+  location = "Poland Central"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "gr-2-vn"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.20.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "gr-2-sn"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.20.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
+resource "azurerm_private_dns_zone" "example" {
+  name                = "example.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_postgresql_flexible_server" "example" {
+  name                          = "grupa-2-psqlflexibleserver"
+  resource_group_name           = azurerm_resource_group.example.name
+  location                      = azurerm_resource_group.example.location
+  version                       = "13"
+  delegated_subnet_id           = azurerm_subnet.example.id
+  private_dns_zone_id           = azurerm_private_dns_zone.example.id
+  public_network_access_enabled = false
+  administrator_login           = "korwin"
+  administrator_password        = "Admin1234$"
+  zone                          = "1"
+
+  storage_mb   = 32768
+  storage_tier = "P30"
+
+  sku_name   = "GP_Standard_D4s_v3"
+
+}
